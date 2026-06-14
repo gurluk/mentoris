84dddac5234c339825388af953d67dc987448f21
Luka Gurdulic <gurdulicluka@pm.me>
2026-06-14 16:12:02 +0200

    drizzle studio command, better auth web package

--- apps/web/src/features/cities/Cities.tsx
+++ apps/web/src/features/cities/Cities.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { Avatar, Card, SimpleGrid, Title } from "@mantine/core";
-
 import { getCities } from "./cities.query";
 
 // import { getCities } from "./cities.query";
@@ -10,16 +8,5 @@ export default function Cities() {
   const { data, error, isLoading } = getCities();
   console.log("🚀 ~ Cities ~ data:", data);
 
-  return (
-    <SimpleGrid cols={{ base: 1, xs: 2, sm: 3, lg: 4 }} mt={50}>
-      <Card padding={"md"} withBorder>
-        <Avatar color="cyan" radius="xl">
-          MK
-        </Avatar>
-        <Title mt={5} order={4}>
-          {data.data[0].label}
-        </Title>
-      </Card>
-    </SimpleGrid>
-  );
+  return <></>;
 }
