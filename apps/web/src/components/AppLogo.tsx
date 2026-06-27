2a3300fcbf116c3a7496c98f35a3ca52741385f9
Luka Gurdulic <gurdulicluka@pm.me>
2026-06-27 18:55:44 +0200

    app placeholder logo navigate root

--- apps/web/src/components/AppLogo.tsx
+++ apps/web/src/components/AppLogo.tsx
@@ -1,9 +1,19 @@
-import { Text } from "@mantine/core";
+"use client";
+
+import { Anchor } from "@mantine/core";
+import Link from "next/link";
 
 export default function AppLogo() {
   return (
-    <Text fz={"xl"} fw={600}>
+    <Anchor
+      c={"dark"}
+      component={Link}
+      href="/"
+      underline="never"
+      fw={500}
+      fz={"xl"}
+    >
       mentoris
-    </Text>
+    </Anchor>
   );
 }
